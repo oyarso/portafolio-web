2b4402dea963f104c489554b66cdae8aaa7f3e4c
oyarso <sam.copae@gmail.com>
2024-07-16 16:26:29 -0400

    Form y mas

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+document.getElementById('esp').style.display = "block";
+document.getElementById('ing').style.display = "none";
+
+function mostrarIngles() {
+    document.getElementById('esp').style.display = "block";
+    document.getElementById('ing').style.display = "none";
+}
+function mostrarEspanol() {
+    document.getElementById('ing').style.display = "block";
+    document.getElementById('esp').style.display = "none";
+}
+
+
+function modoDia() {
+    document.body.style.background = "pink";
+}
+function modoNoche() {
+    document.body.style.background = "olivedrab";
+}
+
